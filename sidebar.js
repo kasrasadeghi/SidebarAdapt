ba8685311ac3ed3cdbd185236475ed4047e59ede
kasrasadeghi <kasrasadeghi@users.noreply.github.com>
2016-12-17 16:02:20 -0600

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         Sidebar Adapt
 // @version      1.0
+// @namespace    https://github.com/kasrasadeghi/SidebarAdapt/
 // @description  Toggle the reddit sidebar and resize post width for vertical monitors.
 // @author       Kasra Sadeghi
 // @match        http*://www.reddit.com/*
 // @require      http://code.jquery.com/jquery-latest.js
 // @grant        none
+// @copyright   2015, cameroncondry
 // ==/UserScript==
 
 $(document).ready(function() {
